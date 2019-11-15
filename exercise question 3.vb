e7be4ae016afc5bc56564a5b76f9fcf767264e3b
ylt1233 <57741838+ylt1233@users.noreply.github.com>
2019-11-15 12:36:44 +0800

    chapter 7 exercise question 3

---
+++ exercise question 3.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim temperature As Integer
+        Dim status As String
+
+        temperature = Val(TextBox1.Text)
+
+        Select Case temperature
+            Case 0 To 30
+                status = "cold"
+            Case 31 To 50
+                status = "medium"
+            Case 51 To 70
+                status = "hot"
+            Case 71 To 100
+                status = "very hot"
+        End Select
+
+        TextBox2.Text = status
+
+    End Sub
+End Class
